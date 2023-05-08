041cd4ddb3988933382355c16cb1c3108f44bc47
BELLRECONzTech <122868815+BELLRECONzTech@users.noreply.github.com>
2023-05-09 00:16:04 +0700

    navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+        <div className='w-full md:h-[10vh] flex justify-between items-center md:px-40 z-100 shadow-lg '>
+            <div className=' flex gap-5  items-center'>
+                <Image 
+                src="/logo.png" 
+                alt=""
+                width={40}
+                height={30} />
+                <Image className=''
+                src="/LaslesVPN.png"  
+                alt=""
+                width={100}
+                height={100}
+                />
+            </div>
+
+        <div className=''>
+            <li className='flex gap-20 items-center'>
+            <ul className='cursor-pointer hover:md:underline text-[#4F5665]'> About </ul>
+            <ul className='cursor-pointer hover:md:underline text-[#4F5665]'>Features</ul>
+            <ul className='cursor-pointer hover:md:underline text-[#4F5665]'>Pricing</ul>
+            <ul className='cursor-pointer hover:md:underline text-[#4F5665]'>Testimonials</ul>
+            <ul className='cursor-pointer hover:md:underline text-[#4F5665]'>Help</ul>
+            </li>
+        </div>
+
+        <div className=' flex items-center gap-10'>
+            <h1 className=' cursor-pointer hover:md:underline text-[24px] text-[#0B132A]'>Sign in</h1>
+            <button className=' relative cursor-pointer border-2 px-6 py-3 border-[#F53855] rounded-[55px] text-[#F53855]'>Sign up</button>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
